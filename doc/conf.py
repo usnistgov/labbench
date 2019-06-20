dd00e8f9e00c6434eaa8362a4d497e7b72a566fe
Dan Kuester <daniel.kuester@nist.gov>
2019-06-20 11:16:29 -0600

    add the NIST disclaimer to the autogenerated doc files

--- doc/conf.py
+++ doc/conf.py
@@ -198,7 +198,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'haiku'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -206,11 +206,11 @@ html_theme = 'haiku'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+# html_theme_path = ['_templates']
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
-#html_title = u'gpslte v'
+html_title = f'labbench v{version}'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None

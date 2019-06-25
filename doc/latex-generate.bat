d15bca8c3a496d54e7434554fe79a52b6ad54a6d
Dan Kuester <daniel.kuester@nist.gov>
2019-06-25 09:23:46 -0600

    more doc

--- doc/latex-generate.bat
+++ doc/latex-generate.bat
@@ -5,6 +5,6 @@ call "%userprofile%\AppData\Local\Continuum\Anaconda2\Scripts\activate.bat" py36
 call "C:\ProgramData\Anaconda2\Scripts\activate.bat"
 call "C:\ProgramData\Anaconda2\Scripts\activate.bat" py36
 
-sphinx-build -b html . ../public
+sphinx-build -b latex . ../public
 
 pause

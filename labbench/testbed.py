12dd224f91868ea34fb81f5e0491200c449fc0a3
Dan Kuester <daniel.kuester@nist.gov>
2020-01-28 13:38:15 -0700

    progress on the higher-level test abstraction piece of this

--- labbench/testbed.py
+++ labbench/testbed.py
@@ -28,36 +28,46 @@ from .util import sequentially,concurrently
 from .data import LogAggregator, RelationalTableLogger
 from .core import Device
 from .host import Host, Email
+from functools import wraps
 
-
-__all__ = ['Testbed']
+__all__ = ['Testbed', 'Steps']
 
 
 class Testbed:
-    """ A collection of Device instances, database managers, and methods that
-        implement an automated experiment.
+    """ A Testbed is a container for devices, data managers, and test steps.
 
-        Use a `with` block with the testbed instance to open everything
-        at once like so::
+        The Testbed object provides connection management for
+        all devices and data managers for `with` block::
 
             with Testbed() as testbed:
                 # use the testbed here
                 pass
 
-        or optionally open only a subset of devices like this::
+        For functional validation, it is also possible to open only a subset
+        of devices like this::
 
             testbed = Testbed()
             with testbed.dev1, testbed.dev2:
                 # use the testbed.dev1 and testbed.dev2 here
                 pass
 
-        Make your own subclass of Testbed with a custom `make`
+        The following syntax creates a new Testbed class for an
+        experiment:
+
+            import labbench as lb
+
+            class MyTestbed(lb.Testbed):
+                db = lb.SQLiteManager()
+                sa = MySpectrumAnalyzer()
+
+                spectrogram = Spectrogram(db=db, sa=sa)
+
         method to define the Device or database manager instances, and
         a custom `startup` method to implement custom code to set up the
         testbed after all Device instances are open.
     """
 
-    # Specify any context manager types to open before others
+    # Specify context manager types to open before others
     # and their order
     enter_first = Email, LogAggregator, Host
 
@@ -173,18 +183,92 @@ class Testbed:
         """
         pass
 
-    class _InTestbed:
-        """ Subclass this in objects that could be context managers in a Testbed
+class _InTestbed:
+    """ Subclass this in objects that could be context managers in a Testbed
 
+    """
+    __owner__ = None
+
+    def __set_name__(self, owner_cls, name):
+        if issubclass(owner_cls, Testbed):
+            owner_cls.__contexts__[name] = self
+
+    def __get__(self):
+        return self
+
+    def __init_owner__(self, owner):
+        self.__owner__ = owner
+
+
+class Steps(Testbed._InTestbed):
+    """ Subclass this to define experimental procedures for groups of Devices in a Testbed.
+    """
+    __annotations__ = dict()
+    __steps__ = dict()
+
+    def __init_subclass__(cls):
+        # By introspection, identify the methods that define test steps
+        cls.__steps__ = dict(((k,v) for k,v in cls.__dict__ if callable(v) and k not in Steps.__dict__))
+
+        # Include annotations from parent classes
+        cls.__annotations__ = dict(super().__annotations__, **cls.__annotations__)
+        cls.__init__.__annotations__ = cls.__annotations__
+
+        # Sentinel values for annotations not in this class
+        for name in cls.__annotations__:
+            if name not in cls.__dict__:
+                setattr(cls, name, None)
+
+    def __init__(self, **devices):
+        # a fresh mapping to modify without changing the parent
+        devices = dict(devices)
+
+        # Set attributes in self
+        for name, devtype in self.__annotations__:
+            try:
+                dev = devices.pop(name)
+            except KeyError:
+                raise KeyError(f"{self.__class__.__qualname__} is missing required argument")
+            if not isinstance(dev, devtype):
+                msg = f"argument '{name}' must be an instance of '{devtype.__qualname__}'"
+                raise AttributeError(msg)
+            setattr(self, name, dev)
+
+        # If there are remaining unsupported devices, raise an exception
+        if len(devices) > 0:
+            raise ValueError(f"{tuple(devices.keys()}} are invalid arguments")
+
+    def __getattribute__(self, name):
+        """ Add debug messages to class method calls
         """
-        __owner__ = None
+        obj = object.__getattribute__(cls, name)
 
-        def __set_name__(self, owner_cls, name):
-            if issubclass(owner_cls, Testbed):
-                owner_cls.__contexts__[name] = self
+        if name in object.__getattribute__(cls, '__steps__'):
+            # TODO: Ensure required devices are connected before executing
+            @wraps(obj)
+            def wrapped(*args, **kws):
+                name = self.__name__ + '.' + obj.__name__
+                lb.logger.debug(f"starting step {name}")
+                with lb.stopwatch(f"step {name}"):
+                    return obj(*args, **kws)
+            return wrapped
+        else:
+            return obj
 
-        def __get__(self):
-            return self
+    def __getitem__(self, item):
+        return self.__steps__[item]
 
-        def __init_owner__(self, owner):
-            self.__owner__ = owner
+    def __len__(self):
+        return len(self.__steps__)
+
+    def __iter__(self):
+        return iter(self.__steps__)
+
+    def items(self):
+        return self.__steps__.items()
+
+    def values(self):
+        return self.__steps__.values()
+
+    def keys(self):
+        return self.__steps__.keys()
